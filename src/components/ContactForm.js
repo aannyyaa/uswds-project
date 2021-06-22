a3e29f94875a9f915e820190ca5576c6130945fd
Anya <anya.dunaif@gmail.com>
2021-06-22 12:56:06 -0400

    form email validation

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -12,7 +12,7 @@ const ContactForm = () => {
   return (
     <Form onSubmit={() => {console.log("Submit!")}}>
       <Label htmlFor="input-email">{t("form.email")}</Label>
-      <TextInput id="input-email" name="input-email" type="text" />
+      <TextInput id="input-email" name="input-email" type="email"/>
       <Label htmlFor="input-subject">{t("form.subject")}</Label>
       <TextInput id="input-subject" name="input-subject" type="text" />
       <Label htmlFor="input-message">{t("form.message")}</Label>
